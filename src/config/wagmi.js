5d502c6dcb10b3f4dd26ef552f9a1cdd91246532
elcharitas <jonathanirhodia@gmail.com>
2022-10-07 06:32:03 +0100

    [chore] add default chain config

--- src/config/wagmi.js
+++ src/config/wagmi.js
@@ -1,9 +1,12 @@
 import { chain } from "wagmi";
 
 /** @type {import('wagmi').Chain[]} */
-const SUPPORTED_CHAINS = [chain.mainnet, chain.polygon, chain.bsc];
+const SUPPORTED_CHAINS = [chain.goerli, chain.polygon];
 
 export const WAGMI_CONFIG = {
     ALCHEMY_ID: process.env.ALCHEMY_ID,
     SUPPORTED_CHAINS,
+    get DEFAULT_CHAIN() {
+        return SUPPORTED_CHAINS[0];
+    },
 };
