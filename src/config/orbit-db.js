b54ba8c5e1f8ca1277198b1612f147df1be0e8ff
elcharitas <jonathanirhodia@gmail.com>
2022-10-11 10:43:02 +0100

    feat: use key/id pair

--- src/config/orbit-db.js
+++ src/config/orbit-db.js
@@ -2,7 +2,6 @@ export const ORBIT_CONFIG = {
     PATHS: process.env.ORBITDB_PATHS?.split(","),
     IDENTITY: {
         ID: process.env.ORBITDB_IDENTITY_ID,
-        PUBLIC_KEY: process.env.ORBITDB_IDENTITY_PUBLIC_KEY,
-        TYPE: process.env.ORBITDB_IDENTITY_TYPE || "orbitdb",
+        KEY: process.env.ORBITDB_IDENTITY_KEY,
     },
 };
