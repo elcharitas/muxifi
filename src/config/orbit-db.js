3b927438d4caf7721aa4a5724d765105359ef8de
elcharitas <jonathanirhodia@gmail.com>
2022-10-11 08:32:15 +0100

    fix: optional path split

--- src/config/orbit-db.js
+++ src/config/orbit-db.js
@@ -1,5 +1,5 @@
 export const ORBIT_CONFIG = {
-    PATHS: process.env.ORBITDB_PATHS.split(","),
+    PATHS: process.env.ORBITDB_PATHS?.split(","),
     IDENTITY: {
         ID: process.env.ORBITDB_IDENTITY_ID,
         PUBLIC_KEY: process.env.ORBITDB_IDENTITY_PUBLIC_KEY,
