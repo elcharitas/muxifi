2715d73f060637a47705e7184803d7417b14e299
elcharitas <jonathanirhodia@gmail.com>
2022-10-15 04:45:06 +0100

    chore: import font source

--- src/providers/theme/ThemeProvider.jsx
+++ src/providers/theme/ThemeProvider.jsx
@@ -1,5 +1,6 @@
 import { CssBaseline, ThemeProvider as MUIThemeProvider } from "@mui/material";
 import { theme } from "./theme-context";
+import "@fontsource/raleway";
 
 const ThemeProvider = ({ children }) => (
     <MUIThemeProvider theme={theme}>
