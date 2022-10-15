bdf0eb38ab35bbf583e3ca70d275a4c47b6e7ab9
elcharitas <jonathanirhodia@gmail.com>
2022-10-15 05:43:48 +0100

    fix: add label props

--- src/components/Anchor.jsx
+++ src/components/Anchor.jsx
@@ -2,19 +2,27 @@ import NextLink from "next/link";
 import Image from "next/image";
 import { Link, Stack, Typography } from "@mui/material";
 
-export const Anchor = ({ children, href, label, icon, sx, ...props }) => {
+export const Anchor = ({
+    children,
+    href,
+    label,
+    icon,
+    sx,
+    labelProps,
+    ...props
+}) => {
     return (
         <NextLink href={href} passHref>
             <Link sx={{ textDecoration: "none", ...sx }} {...props}>
-                <Stack direction="row" spacing={1}>
+                <Stack direction="row" alignItems="center" spacing={1}>
                     {icon && (
                         <Image
                             src={`/assets/svgs/${icon}.svg`}
-                            width={27}
-                            height={27}
+                            width={26}
+                            height={26}
                         />
                     )}
-                    {label && <Typography>{label}</Typography>}
+                    {label && <Typography {...labelProps}>{label}</Typography>}
                     {children}
                 </Stack>
             </Link>
