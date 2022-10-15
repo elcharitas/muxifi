1c2adcbd86f8f8005899c78d25f66d4c94c0490c
elcharitas <jonathanirhodia@gmail.com>
2022-10-15 04:05:53 +0100

    refactor: merge styles into layout

--- src/layouts/app/index.jsx
+++ src/layouts/app/index.jsx
@@ -1,5 +1,13 @@
 import Head from "next/head";
-import { RootStyle } from "./index.style";
+import { styled } from "@mui/material";
+
+export const RootStyle = styled("div")(({ theme }) => ({
+    display: "flex",
+    minHeight: "100%",
+    overflow: "hidden",
+    position: "relative",
+    backgroundColor: theme.palette.background.default,
+}));
 
 const AppLayout = ({ title = "", children }) => {
     return (
--- src/layouts/app/index.style.jsx
+++
@@ -1,9 +0,0 @@
-const { styled } = require("@mui/material");
-
-export const RootStyle = styled("div")(({ theme }) => ({
-    display: "flex",
-    minHeight: "100%",
-    overflow: "hidden",
-    position: "relative",
-    backgroundColor: theme.palette.background.default,
-}));
