4432baa1cbc38f82d6bb629b64a298c432e0ac6b
elcharitas <jonathanirhodia@gmail.com>
2022-10-15 04:44:57 +0100

    chore: set default font

--- src/config/theme.js
+++ src/config/theme.js
@@ -4,7 +4,7 @@ export const THEME_CONFIG = {
     DEFAULT_THEME: "dark",
     // The default color is gold (FFA611), but we can change it
     DEFAULT_COLOR: "#FFA611",
-    // The default font is Nunito, but we can change it to
+    // The default font is Raleway, but we can change it to
     // another font by changing this value
-    DEFAULT_FONT: "Nunito",
+    DEFAULT_FONT: "Raleway",
 };
