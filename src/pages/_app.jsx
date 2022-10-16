1c97539190db1ee768a2b534520e11db37d9d263
elcharitas <jonathanirhodia@gmail.com>
2022-10-16 04:31:06 +0100

    fix: force rainbowkit to default font

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,6 +4,11 @@ const MuxiApp = ({ Component, pageProps }) => {
     return (
         <ThemeProvider>
             <AuthProvider>
+                <style>{`
+                    [data-rk]{
+                        --rk-fonts-body: "Raleway", sans-serif !important;
+                    }
+                `}</style>
                 <Component {...pageProps} />
             </AuthProvider>
         </ThemeProvider>
