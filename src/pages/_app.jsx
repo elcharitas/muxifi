983faff03343962e28721e4c002540d917ea3f0d
elcharitas <jonathanirhodia@gmail.com>
2022-10-16 04:43:32 +0100

    chore: cleanup

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,11 @@
+import { CONFIG } from "src/config";
 import { AuthProvider, ThemeProvider } from "src/providers";
 
 const MuxiApp = ({ Component, pageProps }) => {
     return (
         <ThemeProvider>
             <AuthProvider>
-                <style>{`
-                    [data-rk]{
-                        --rk-fonts-body: "Raleway", sans-serif !important;
-                    }
-                `}</style>
+                <style>{`[data-rk]{ --rk-fonts-body: "${CONFIG.THEME.DEFAULT_FONT}, sans-serif !important;}`}</style>
                 <Component {...pageProps} />
             </AuthProvider>
         </ThemeProvider>
