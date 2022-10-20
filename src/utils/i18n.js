8e300cc51ba8fe347be8d493ce54f08dc1b6c3ca
elcharitas <jonathanirhodia@gmail.com>
2022-10-20 19:50:03 +0100

    chore: implement translations

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,5 +1,5 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export const buildI18n = (locale, ns) => {
-    return serverSideTranslations(locale, ["common", ...ns]);
+    return serverSideTranslations(locale, ["common", "account", ...ns]);
 };
