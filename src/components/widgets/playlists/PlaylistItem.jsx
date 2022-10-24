c4203db1dd5572647b9a9e4e42e0988d9304090d
elcharitas <jonathanirhodia@gmail.com>
2022-10-24 07:17:40 +0100

    feat: add playlist action button

--- src/components/widgets/playlists/PlaylistItem.jsx
+++ src/components/widgets/playlists/PlaylistItem.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Grid, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { RootWidgetStyle } from "src/components/styles";
+import { PlayButton } from "src/components/PlayButton";
 import Img from "src/assets/img/trialImg.png";
 
 const Textbox = styled(Typography)({
@@ -16,6 +17,9 @@ export const PlaylistItem = ({ title }) => (
         <RootWidgetStyle sx={{ width: { xs: "100%", md: 300 } }}>
             <Image alt="" src={Img} width="70" height="70" />
             <Textbox>{title}</Textbox>
+            <div className="root-btn">
+                <PlayButton />
+            </div>
         </RootWidgetStyle>
     </Grid>
 );
