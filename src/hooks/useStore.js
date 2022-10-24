21c392cec8d9fcb65dc516afd06a8f6707c9e4df
elcharitas <jonathanirhodia@gmail.com>
2022-10-24 19:56:52 +0100

    chore: improve store with mount options

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,7 +1,8 @@
-import { useLocalStorage } from "usehooks-ts";
+import { useLocalStorage, useIsMounted, useIsClient } from "usehooks-ts";
 
 /** Initial state of the app's store */
 const INITIAL_STATE = {
+    appVersion: "1.0.0",
     currentTrack: {
         id: 0,
         playlist: 0,
@@ -16,8 +17,10 @@ const INITIAL_STATE = {
  * @param {keyof INITIAL_STATE} path
  * @returns
  */
-export const useStore = (path) => {
+export const useStore = (path = "appVersion") => {
     const [store, setStore] = useLocalStorage("muxifi", INITIAL_STATE);
+    const isMounted = useIsMounted();
+    const isClient = useIsClient();
 
     const dispatch = (key, value) => {
         setStore((prevState) => ({
@@ -32,20 +35,19 @@ export const useStore = (path) => {
         store,
         dispatch,
         reset,
-        ...(path && {
-            [path]: store[path],
-            /**
-             * Setter for the current path
-             *
-             * @param {keyof store[path]} key
-             * @param {store[path][key]} value
-             */
-            set(key, value) {
-                this.dispatch(path, {
-                    ...store[path],
-                    [key]: value,
-                });
-            },
-        }),
+        [path]: store[path],
+        /**
+         * Setter for the current path
+         *
+         * @param {keyof store[path]} key
+         * @param {store[path][key]} value
+         */
+        set(key, value) {
+            this.dispatch(path, {
+                ...store[path],
+                [key]: value,
+            });
+        },
+        ready: isMounted && isClient,
     };
 };
