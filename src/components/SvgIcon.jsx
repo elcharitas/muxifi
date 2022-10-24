f14ad7e187826a920a15fd1368de0c220f84d000
elcharitas <jonathanirhodia@gmail.com>
2022-10-24 06:22:02 +0100

    feat: add optional dim props to svg icons

--- src/components/SvgIcon.jsx
+++ src/components/SvgIcon.jsx
@@ -1,6 +1,13 @@
 import dynamic from "next/dynamic";
 
-export const SvgIcon = ({ name, size = "32", color, ...props }) => {
+export const SvgIcon = ({
+    name,
+    size = "32",
+    color,
+    width,
+    height,
+    ...props
+}) => {
     const Icon = dynamic(() => import(`src/assets/svgs/${name}.svg`));
-    return <Icon width={size} height={size} {...props} />;
+    return <Icon width={width || size} height={height || size} {...props} />;
 };
