5a50e072253359b99b5ea2ca082a99f012a39e96
elcharitas <jonathanirhodia@gmail.com>
2022-10-28 03:23:58 +0100

    chore: export default paths

--- src/config/orbit-db.js
+++ src/config/orbit-db.js
@@ -1,7 +1,10 @@
 export const ORBIT_CONFIG = {
-    PATHS: process.env.ORBITDB_PATHS?.split(","),
+    PATHS: process.env.ORBITDB_PATHS?.split(",") || [],
     IDENTITY: {
         ID: process.env.ORBITDB_IDENTITY_ID,
         KEY: process.env.ORBITDB_IDENTITY_KEY,
+    },
+    get DEFAULT_PATH() {
+        return this.PATHS[0];
     },
 };
