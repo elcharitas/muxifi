1a78c31461ac5222406cdc57ebdb37de78a49f1a
elcharitas <jonathanirhodia@gmail.com>
2022-10-28 06:04:09 +0100

    feat: rewrite album contract logic

--- contracts/core/MuxifiAlbum.sol
+++ contracts/core/MuxifiAlbum.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 /**
  * @title MuxifiAlbum
@@ -12,31 +13,53 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
  * Songs in MuxifiAlbums can be burned by the owner.
  */
 contract MuxifiAlbum is ERC1155, ERC1155Burnable {
-    // max number of songs in this album
-    uint8 public immutable MAX_SONGS;
+    using Counters for Counters.Counter;
+    Counters.Counter private _albumIds;
 
-    constructor(address owner, uint8 count)
-        ERC1155("https://Muxifi.vercel.app/api/albums/{id}.json")
-    {
-        require(count > 0, "Album must have at least one song");
+    address public immutable muxifi;
+    uint256 public immutable mintFee;
 
-        // set max songs
-        MAX_SONGS = count;
+    struct AlbumDetail {
+        address owner;
+        string metaCID;
+    }
 
-        // mint songs to owner
-        _mintSongs(owner);
+    mapping(uint256 => AlbumDetail) public metaData;
 
-        // allow Muxifi to sell songs on behalf of owner
-        _setApprovalForAll(owner, msg.sender, true);
+    constructor(address _muxifi, uint256 _mintFee) ERC1155("") {
+        muxifi = _muxifi;
+        mintFee = _mintFee;
     }
 
     /**
-     * @dev Mint songs for the owner.
-     * @param owner The address of the owner of the songs.
+     * @dev create an album
      */
-    function _mintSongs(address owner) internal {
-        for (uint8 songId = 0; songId < MAX_SONGS; i++) {
-            _mint(owner, songId, 1 ether, "");
-        }
+    function create(string memory _metaCID) external payable returns (uint256) {
+        require(
+            msg.value >= mintFee,
+            "Sorry insufficient amount to create album"
+        );
+
+        uint256 albumId = _albumIds.current();
+        metaData[albumId] = AlbumDetail(msg.sender, _metaCID);
+
+        _mint(msg.sender, albumId, 1 ether, "");
+
+        return albumId;
+    }
+
+    /**
+     * @dev called by an external contract to remove fees paid in
+     */
+    function withdraw() external {
+        require(
+            msg.sender == muxifi,
+            "You are not authorised to call this function"
+        );
+        (bool success, ) = payable(muxifi).call{value: address(this).balance}(
+            ""
+        );
+
+        require(success, "Something went wrong while withdrawing");
     }
 }
