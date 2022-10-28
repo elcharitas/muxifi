0214e9dab473eb089ca0b4c5360029bbf2c83bab
elcharitas <jonathanirhodia@gmail.com>
2022-10-28 05:34:55 +0100

    chore: rename muxifi album

--- contracts/core/MuxifiAlbum.sol
+++ contracts/core/MuxifiAlbum.sol
@@ -5,18 +5,18 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
 /**
- * @title MuxifyAlbum
- * @dev MuxifyAlbum is a contract for managing a collection of songs.
- * MuxifyAlbums are ERC1155s that can be minted and sold.
- * MuxifyAlbums are minted by Muxify and sold on behalf of the owner.
- * Songs in MuxifyAlbums can be burned by the owner.
+ * @title MuxifiAlbum
+ * @dev MuxifiAlbum is a contract for managing a collection of songs.
+ * MuxifiAlbums are ERC1155s that can be minted and sold.
+ * MuxifiAlbums are minted by Muxifi and sold on behalf of the owner.
+ * Songs in MuxifiAlbums can be burned by the owner.
  */
-contract MuxifyAlbum is ERC1155, ERC1155Burnable {
+contract MuxifiAlbum is ERC1155, ERC1155Burnable {
     // max number of songs in this album
     uint8 public immutable MAX_SONGS;
 
     constructor(address owner, uint8 count)
-        ERC1155("https://muxify.vercel.app/api/albums/{id}.json")
+        ERC1155("https://Muxifi.vercel.app/api/albums/{id}.json")
     {
         require(count > 0, "Album must have at least one song");
 
@@ -26,7 +26,7 @@ contract MuxifyAlbum is ERC1155, ERC1155Burnable {
         // mint songs to owner
         _mintSongs(owner);
 
-        // allow Muxify to sell songs on behalf of owner
+        // allow Muxifi to sell songs on behalf of owner
         _setApprovalForAll(owner, msg.sender, true);
     }
 
