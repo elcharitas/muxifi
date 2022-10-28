e4f00193175edd1e3652d018329e0eba79f0f8b0
elcharitas <jonathanirhodia@gmail.com>
2022-10-28 18:49:35 +0100

    feat: track supply of albums

--- contracts/core/MuxifiAlbum.sol
+++ contracts/core/MuxifiAlbum.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./MuxifiCreator.sol";
 
@@ -12,7 +13,7 @@ import "./MuxifiCreator.sol";
  * MuxifiAlbums are minted by Muxifi and sold on behalf of the owner.
  * Songs in MuxifiAlbums can be burned by the owner.
  */
-contract MuxifiAlbum is ERC1155URIStorage {
+contract MuxifiAlbum is ERC1155URIStorage, ERC1155Supply {
     using Counters for Counters.Counter;
     Counters.Counter private _albumIds;
 
@@ -51,6 +52,7 @@ contract MuxifiAlbum is ERC1155URIStorage {
 
         _setURI(albumId, _metaCID);
         _mint(msg.sender, albumId, 1 ether, "");
+        _albumIds.increment();
 
         return albumId;
     }
@@ -68,5 +70,32 @@ contract MuxifiAlbum is ERC1155URIStorage {
         );
 
         require(success, "Something went wrong while withdrawing");
+    }
+
+    /**
+     * @dev gets count of albums
+     */
+    function count() external view returns (uint256) {
+        return _albumIds.current() + 1;
+    }
+
+    function uri(uint256 _albumId)
+        public
+        view
+        override(ERC1155, ERC1155URIStorage)
+        returns (string memory)
+    {
+        return super.uri(_albumId);
+    }
+
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal override(ERC1155, ERC1155Supply) {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 }
