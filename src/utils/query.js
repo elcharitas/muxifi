1b44c4ff61e30f4e2ac44fe52ec7c39ece43aca3
elcharitas <jonathanirhodia@gmail.com>
2022-10-29 17:54:34 +0100

    feat: add query to get nft metadata

--- src/utils/query.js
+++ src/utils/query.js
@@ -2,7 +2,12 @@ import Moralis from "moralis";
 import { CONFIG } from "src/config";
 
 const evmApi = Moralis.EvmApi;
-const { getContractNFTs, getWalletNFTs, searchNFTs } = evmApi.nft;
+const {
+    getContractNFTs,
+    getWalletNFTs,
+    getNFTMetadata,
+    searchNFTs,
+} = evmApi.nft;
 
 const { CONTRACT_ADDRESSES } = CONFIG.WAGMI;
 
@@ -15,6 +20,15 @@ export const getAlbumsQuery = async ({
         limit: pageSize,
     });
     return { pagination, next, result: result.map((r) => r.toJSON()) };
+};
+
+export const getAlbumMetaQuery = async ({ address, id }) => {
+    const { result } = await getNFTMetadata({
+        address,
+        tokenId: id,
+        chain: CONFIG.WAGMI.DEFAULT_CHAIN,
+    });
+    return result.toJSON();
 };
 
 export const getAccountAlbumsQuery = async ({
