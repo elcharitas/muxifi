c0f9e835eadb562a9a1bec11ad5f0ecc14e512d3
elcharitas <jonathanirhodia@gmail.com>
2022-10-29 08:18:56 +0100

    feat: add contract address configs

--- src/config/network.js
+++ src/config/network.js
@@ -3,7 +3,15 @@ import { chain } from "wagmi";
 /** @type {import('wagmi').Chain[]} */
 const SUPPORTED_CHAINS = [chain.goerli, chain.polygon];
 
+const CONTRACT_ADDRESSES = {
+    main: process.env.MUXIFI_ADDRESS,
+    creator: process.env.MUXIFI_CREATOR_ADDRESS,
+    playlist: process.env.MUXIFI_ALBUM_ADDRESS,
+    market: process.env.MUXIFI_MARKET_ADDRESS,
+};
+
 export const WAGMI_CONFIG = {
+    CONTRACT_ADDRESSES,
     ALCHEMY_ID: process.env.ALCHEMY_ID,
     SUPPORTED_CHAINS,
     get DEFAULT_CHAIN() {
