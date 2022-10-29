58ffdf26bb5d6a1eb0f4a4d7c06a45d35fd74af1
elcharitas <jonathanirhodia@gmail.com>
2022-10-29 01:42:30 +0100

    feat: add payout options to muxifi

--- contracts/Muxify.sol
+++ contracts/Muxify.sol
@@ -1,53 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
-import "./core/MuxifyCreator.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./core/MuxifiAlbum.sol";
 
 /**
- * @title Muxify
- * @dev Muxify is a contract for managing a collection of albums.
- * MuxifyAlbums are ERC1155s that can be minted and sold.
- * MuxifyAlbums are minted by Muxify and sold on behalf of the owner.
- * MuxifyAlbums are sold for a fixed price of 1 ETH.
+ * @title Muxifi
+ * @dev Muxifi is a contract for managing a collection of albums.
  */
-contract Muxify {
-    address private immutable MUXIFY_CREATOR;
-
-    constructor(address creator) {
-        MUXIFY_CREATOR = creator;
+contract Muxifi is Ownable {
+    /**
+     * @dev Function called to remove balance found in NFT Contract.
+     * Ideally, MuxiFi address should be used to create the album contract
+     */
+    function claim(address _album) external onlyOwner {
+        MuxifiAlbum(_album).withdraw();
     }
 
     /**
-     * @dev Collect songs from an album.
-     * @param album The address of the album.
-     * @param songIds The id of the song.
-     * @param amounts The amount of songs to collect.
+     * @dev This function is used to payout admins, marketers, developers, etc
      */
-    function collectSongs(
-        address album,
-        uint256[] memory songIds,
-        uint256[] memory amounts
-    ) external payable {
+    function payout(address _who, uint256 _amount) external onlyOwner {
         require(
-            MuxifyCreator(MUXIFY_CREATOR).splitter[album] != address(0),
-            "Album does not exist"
+            _amount <= address(this).balance,
+            "Sorry, but contract balance is lower than this amount"
         );
-        require(songIds.length > 0, "No songs to collect");
-        require(songIds.length == amounts.length, "Invalid songIds or amounts");
+        (bool success, ) = payable(_who).call{value: _amount}("");
 
-        PaymentSplitter splitterContract = PaymentSplitter(
-            payable(MuxifyCreator(MUXIFY_CREATOR).splitter[album])
-        );
-
-        // collect the songs from the album
-        MuxifyAlbum(album).safeBatchTransferFrom(
-            splitterContract.payee(1),
-            msg.sender,
-            songIds,
-            amounts,
-            ""
-        );
+        require(success, "Something went wrong while paying out");
     }
 
     fallback() external payable {
