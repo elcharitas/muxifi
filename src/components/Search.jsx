93d4b3a8d5e1161809fcb7bc99ea0284399bc8ba
elcharitas <jonathanirhodia@gmail.com>
2022-10-30 04:47:52 +0100

    chore: use theme vars

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,15 +2,16 @@ import { Box, styled } from "@mui/material";
 import { SearchInput } from "src/components/styles";
 import { SvgIcon } from "./SvgIcon";
 
-const SearchBox = styled(Box)({
-    border: "1px dashed #565D5A",
+const SearchBox = styled(Box)(({ theme }) => ({
+    border: "1px dashed",
+    borderColor: theme.palette.tertiary.main,
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
     width: 300,
     padding: "10px 14px",
     borderRadius: 48,
-});
+}));
 
 export const Search = ({ sx, ...props }) => {
     return (
