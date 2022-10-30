80a8e12d5c1175c8746f8e7b7c704a5ddf779ad1
Oluwayomi Oke <okeoluwayomi9@gmail.com>
2022-10-30 18:27:34 +0100

    feat: create albums page

---
+++ src/pages/app/albums.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import AppLayout from "src/layouts/app";
+import { buildI18n } from "src/utils/i18n";
+import { Box, Grid } from "@mui/material";
+import { Heading } from "src/components";
+import { RootStyle } from "src/components/styles";
+import { PlaylistCard } from "src/components/widgets";
+import AlbumImg from "src/assets/img/albumImg.png";
+
+export const getStaticProps = async ({ locale }) => ({
+    props: {
+        ...(await buildI18n(locale, ["playlist"])),
+    },
+});
+
+const ArtistePage = () => {
+    return (
+        <AppLayout title="Artistes">
+            <RootStyle>
+                <Box>
+                    <Heading
+                        sx={{ mb: "24px" }}
+                        title="Albums"
+                        size="modal-title"
+                    />
+
+                    <Grid container spacing="18px">
+                        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(
+                            (item) => (
+                                <Grid
+                                    item
+                                    xs={12}
+                                    sm={6}
+                                    md={4}
+                                    lg={3}
+                                    key={item}
+                                >
+                                    <PlaylistCard
+                                        title="Album name"
+                                        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                                        image={AlbumImg}
+                                        isCollected
+                                    />
+                                </Grid>
+                            ),
+                        )}
+                    </Grid>
+                </Box>
+            </RootStyle>
+        </AppLayout>
+    );
+};
+
+export default ArtistePage;
