74fd25f670ad6759058de82eb53ad8321338191e
elcharitas <jonathanirhodia@gmail.com>
2022-10-31 21:02:09 +0100

    fix: collection card display

--- src/components/collections/CollectionCard.jsx
+++ src/components/collections/CollectionCard.jsx
@@ -1,35 +1,32 @@
-import { Grid, Typography } from "@mui/material";
-// import { PlayButton } from "src/components/ControlButton";
+import { Box, Typography } from "@mui/material";
+import { PlayButton } from "src/components/ControlButton";
+import { RootWidgetStyle } from "../styles";
 
 export const CollectionCard = ({ title, sx }) => {
     return (
-        <Grid
-            item
-            xs={12}
-            sm={12}
-            lg={6}
+        <RootWidgetStyle
             sx={{
                 backgroundColor: "#9747FF",
-                borderRadius: "12px",
-                padding: "48px",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "flex-end",
-                alignItems: "flex-start",
+                justifyContent: "space-between",
+                alignItems: "flex-end",
                 minHeight: 250,
+                minWidth: { xs: "100%", md: 500 },
                 ...sx,
             }}
         >
-            <Typography variant="modal-title">{title}
-            </Typography>
-            <Typography
-                sx={{ fontSize: "16.67px", fontWeight: 500 }}
-            >125 Liked Songs
-            </Typography>
-
-            {/* <div className="root-btn">
-                <PlayButton onClick={handlePlay} />
-            </div> */}
-        </Grid>
+            <Box
+                sx={{
+                    mb: 4,
+                }}
+            >
+                <Typography variant="modal-title">{title}</Typography>
+                <Typography sx={{ fontWeight: 500 }}>
+                    125 Liked Songs
+                </Typography>
+            </Box>
+            <div className="root-btn">
+                <PlayButton />
+            </div>
+        </RootWidgetStyle>
     );
 };
