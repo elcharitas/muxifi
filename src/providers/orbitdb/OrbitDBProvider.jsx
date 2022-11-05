62c66868cbfe37400a5b2ba1335a77ecaec0a477
elcharitas <jonathanirhodia@gmail.com>
2022-11-05 18:54:32 +0100

    fix: add ipfs config

--- src/providers/orbitdb/OrbitDBProvider.jsx
+++ src/providers/orbitdb/OrbitDBProvider.jsx
@@ -2,20 +2,34 @@ import React, { useEffect, useState } from "react";
 import { useIPFS, useOrbitInstance } from "src/hooks";
 import { orbit } from "./orbit-context";
 
+const IPFS_CONFIG = {
+    start: true,
+    relay: {
+        enabled: false, // enable relay dialer/listener (STOP)
+        hop: {
+            enabled: false, // make this node a relay (HOP)
+        },
+    },
+    preload: {
+        enabled: false,
+    },
+    EXPERIMENTAL: { pubsub: true },
+    config: {
+        Bootstrap: [
+            "/dns4/ipfs-ws.vps.revolunet.com/tcp/443/wss/ipfs/QmSEbJSiV8TXyaG9oBJRs2sJ5sttrNQJvbSeGe7Vt8ZBqt",
+        ],
+        Addresses: {
+            Swarm: [
+                "/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star",
+                "/dns4/star-signal.cloud.ipfs.team/tcp/443/wss/p2p-webrtc-star",
+                "/dns4/libp2p-rdv.vps.revolunet.com/tcp/443/wss/p2p-webrtc-star",
+            ],
+        },
+    },
+};
+
 const OrbitDBProvider = ({ children }) => {
-    const ipfsInstance = useIPFS({
-        start: true,
-        relay: {
-            enabled: false, // enable relay dialer/listener (STOP)
-            hop: {
-                enabled: false, // make this node a relay (HOP)
-            },
-        },
-        preload: {
-            enabled: false,
-        },
-        EXPERIMENTAL: { pubsub: true },
-    });
+    const ipfsInstance = useIPFS(IPFS_CONFIG);
     const orbitInstance = useOrbitInstance(ipfsInstance);
 
     const [value, setValue] = useState(null);
