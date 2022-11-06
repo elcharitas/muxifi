10e7d9ab3f9a5d072ca19c54e1a455294a19a00f
elcharitas <jonathanirhodia@gmail.com>
2022-11-06 18:01:57 +0100

    feat: create coming soon page

---
+++ src/pages/app/coming-soon.jsx
@@ -0,0 +1,19 @@
+import AppLayout from "src/layouts/app";
+import { buildI18n } from "src/utils/i18n";
+import { Heading } from "src/components";
+
+export const getStaticProps = async ({ locale }) => ({
+    props: {
+        ...(await buildI18n(locale, ["playlist"])),
+    },
+});
+
+const ComingSoonPage = () => {
+    return (
+        <AppLayout title="Coming Soon">
+            <Heading title="Coming Soon" />
+        </AppLayout>
+    );
+};
+
+export default ComingSoonPage;
