82f13ad53c5cb1c5e0385875bc88c977378da06b
elcharitas <jonathanirhodia@gmail.com>
2022-11-08 15:02:15 +0100

    fix: studio display and language

--- src/pages/app/studio.jsx
+++ src/pages/app/studio.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useImmer } from "use-immer";
 import { Box, TextField } from "@mui/material";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
-import { pascal } from "radash";
+import { useTranslation } from "next-i18next";
 import AppLayout from "src/layouts/app";
 import { buildI18n } from "src/utils/i18n";
 import { Button, Heading } from "src/components";
@@ -10,10 +10,12 @@ import { useCollection, useCollectionRead, useNFTStorage } from "src/hooks";
 import { FileUpload } from "src/components/FileUpload";
 import { useAccount } from "wagmi";
 import { CreatorModal } from "src/components/widgets/studio/CreatorModal";
+import { ItemCard } from "src/components/widgets";
+import PlaylistImg from "src/assets/img/trial2.png";
 
 export const getStaticProps = async ({ locale }) => ({
     props: {
-        ...(await buildI18n(locale, ["playlist"])),
+        ...(await buildI18n(locale, ["studio"])),
     },
 });
 
@@ -27,6 +29,7 @@ const ALBUM_FIELDS = {
 
 const StudioPage = () => {
     const { address } = useAccount();
+    const { t } = useTranslation("studio");
     const { openConnectModal } = useConnectModal();
     const [album, dispatch] = useImmer({
         title: "",
@@ -59,70 +62,86 @@ const StudioPage = () => {
     }, [writeAsync]);
 
     return (
-        <AppLayout title="Creator Studio">
+        <AppLayout title={t("page.title")}>
             <Heading
                 sx={{ mb: 6 }}
-                title="Creator Studio"
-                subtitle="Mint/Upload a new Album to Muxifi"
+                title={t("page.title")}
+                subtitle={t("page.subtitle")}
                 size="modal-title"
             />
-            <Box
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "20px",
-                    maxWidth: "400px",
-                }}
-            >
-                {Object.entries(ALBUM_FIELDS).map(([key, type]) => {
-                    if (type === "file") {
+            <Box sx={{ display: "flex" }}>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "20px",
+                        width: "100%",
+                        maxWidth: "400px",
+                    }}
+                >
+                    {Object.entries(ALBUM_FIELDS).map(([key, type]) => {
+                        if (type === "file") {
+                            return (
+                                <FileUpload
+                                    key={key}
+                                    label={t(`form.${key}`)}
+                                    exts={
+                                        key === "audio"
+                                            ? ["mp3", "wav"]
+                                            : undefined
+                                    }
+                                    onChange={(value) => {
+                                        dispatch((draft) => {
+                                            // eslint-disable-next-line no-param-reassign
+                                            draft[key] = value;
+                                        });
+                                    }}
+                                />
+                            );
+                        }
                         return (
-                            <FileUpload
+                            <TextField
                                 key={key}
-                                label={pascal(key)}
-                                exts={
-                                    key === "audio" ? ["mp3", "wav"] : undefined
-                                }
-                                onChange={(value) => {
+                                label={t(`form.${key}`)}
+                                value={album[key]}
+                                onChange={(e) => {
                                     dispatch((draft) => {
                                         // eslint-disable-next-line no-param-reassign
-                                        draft[key] = value;
+                                        draft[key] = e.target.value;
                                     });
                                 }}
+                                multiline={key === "description"}
+                                rows={key === "description" ? 4 : undefined}
+                                fullWidth
                             />
                         );
-                    }
-                    return (
-                        <TextField
-                            key={key}
-                            label={pascal(key)}
-                            value={album[key]}
-                            onChange={(e) => {
-                                dispatch((draft) => {
-                                    // eslint-disable-next-line no-param-reassign
-                                    draft[key] = e.target.value;
-                                });
-                            }}
-                            multiline={key === "description"}
-                            rows={key === "description" ? 4 : undefined}
-                            fullWidth
-                        />
-                    );
-                })}
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => {
-                        setAlbumData({
-                            name: album.name,
-                            description: album.description,
-                            image: album.image,
-                        });
-                    }}
-                    isLoading={!!albumData}
-                >
-                    Mint Album
-                </Button>
+                    })}
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => {
+                            if (!album.image) return;
+                            setAlbumData({
+                                name: album.name,
+                                description: album.description,
+                                image: album.image,
+                                address,
+                            });
+                        }}
+                        isLoading={!!albumData}
+                    >
+                        {t("form.submit")}
+                    </Button>
+                </Box>
+                <Box sx={{ ml: 4 }}>
+                    <ItemCard
+                        id="new"
+                        title={album.title || "------"}
+                        desc={album.description || "--------"}
+                        image={album.image ? album.image : PlaylistImg}
+                        owner={address}
+                    />
+                </Box>
             </Box>
             {creatorId !== undefined && !creatorId && (
                 <CreatorModal
