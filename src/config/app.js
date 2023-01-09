ee89819ead5f1237dbf379c4aa7ef5a3796ea162
elcharitas <jonathanirhodia@gmail.com>
2023-01-09 12:54:40 +0100

    fix: ud redirect url

--- src/config/app.js
+++ src/config/app.js
@@ -1,11 +1,15 @@
+const APP_URL = String(
+    process.env.VERCEL_ENV !== "development"
+        ? "https://muxifi.com"
+        : `http://${process.env.VERCEL_URL}`,
+);
+
 export const APP_CONFIG = {
     NAME: "Muxify",
     VERSION: "0.1.0",
     DESCRIPTION: "Muxify is a music player for the web",
     COLLECTIONS: ["albums", "artistes", "podcasts", "playlists"],
-    URL: `http${process.env.VERCEL_ENV !== "development" && "s"}://${
-        process.env.VERCEL_URL
-    }`,
+    URL: APP_URL,
     SOCIAL: {
         MESSAGE:
             "https://docs.google.com/forms/d/e/1FAIpQLSdE2W2xfDEDQbWJ6KJGr6vrjSHCSg8Dg1BOqvYxlPb78CW3Jw/viewform",
