92c6af22c579220a60c0fa9ec268e528eba5ae6b
elcharitas <jonathanirhodia@gmail.com>
2023-01-09 09:31:24 +0100

    feat: create ud wallet integration

---
+++ src/utils/udWallet.js
@@ -0,0 +1,46 @@
+import UAuth from "@uauth/js";
+import { CONFIG } from "src/config";
+
+const uauth = new UAuth({
+    clientID: CONFIG.WAGMI.UNSTOPPABLE_ID,
+    redirectUri: CONFIG.APP.URL,
+    scope: "openid wallet",
+});
+
+const UD_CONFIG = {
+    id: "unstoppable",
+    theme: "#0c2f78",
+    icon:
+        "https://storage.googleapis.com/unstoppable-client-assets/images/favicon/icon.svg",
+};
+
+export const udWallet = () => ({
+    id: UD_CONFIG.id,
+    name: "Login with Unstoppable",
+    iconUrl: UD_CONFIG.icon,
+    iconBackground: UD_CONFIG.theme,
+    createConnector: () => ({
+        connector: {
+            id: UD_CONFIG.id,
+            ready: true,
+            connect() {
+                return JSON.parse(localStorage.getItem("unstoppable"));
+            },
+            disconnect() {
+                localStorage.removeItem("unstoppable");
+                return uauth.logout();
+            },
+            getAccount() {
+                return this.connect()?.account;
+            },
+            async getChainId() {
+                const auth = await uauth.loginWithPopup();
+                const ud = JSON.stringify({
+                    account: auth.idToken.wallet_address,
+                });
+                localStorage.setItem("unstoppable", ud);
+            },
+            on() {},
+        },
+    }),
+});
